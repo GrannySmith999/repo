4ec2ba594912e26168b0853ea208aae581fa9710
GrannySmith999 <soulselfcoach@gmail.com>
2025-09-26 09:41:04 +0100

    Update init.js

--- init.js
+++ init.js
@@ -1,12 +1,14 @@
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyDYcHahEotRafzgRUXLyx1-hDM_WkxdZhs",
-    authDomain: "review-master-app-6aeeb.firebaseapp.com",
-    databaseURL: "https://review-master-app-6aeeb-default-rtdb.firebaseio.com",
-    projectId: "review-master-app-6aeeb",
-    storageBucket: "review-master-app-6aeeb.appspot.com",
-    messagingSenderId: "610185397699",
-    appId: "1:610185397699:web:29ae6847033161c196e976"
+    apiKey: "AIzaSyA3-nTaUBoevijH4Ajt8HDG6_ouD3vjt7Q",
+    authDomain: "taskmanager-88b83.firebaseapp.com",
+    // This is the correct URL for your new Realtime Database
+    databaseURL: "https://taskmanager-88b83-default-rtdb.firebaseio.com/", 
+    projectId: "taskmanager-88b83",
+    storageBucket: "taskmanager-88b83.appspot.com",
+    messagingSenderId: "915611634915",
+    appId: "1:915611634915:web:06f51aafb6de38877109d6",
+    measurementId: "G-TZSGXEFJLT"
 };
 
 // Initialize Firebase only if it hasn't been initialized yet
